ab095d12a76223564cbb94d834e2342d9d99ee99
nithinmudhiraj7799 <talarinithin7799>
2024-07-04 23:22:53 +0530

    buttonComponentCreat

--- src/App.js
+++ src/App.js
@@ -1,9 +1,16 @@
+import ButtonComponent from "./components/button/button";
 import FormData from "./components/functional-based/fetchingTable/dataform";
 
 function App(){
   return(
     <>
-  <FormData/>
+  {/* <FormData/>   */}
+
+  {/* <h1>hai nithin</h1>
+  {
+    [1].map((num)=>(<ButtonComponent/> ))
+  } */}
+
     </>
   )
 }
---
+++ src/components/button/button.js
@@ -0,0 +1,6 @@
+const ButtonComponent=()=>{
+    return(
+        <button onClick={()=>{}} className="">click me</button>
+    )
+}
+export default ButtonComponent;
